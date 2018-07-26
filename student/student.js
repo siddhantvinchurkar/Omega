d6883b5faa7be1abbf66ad1f8430f09812bf47f9
Nikhil <nik98hil@gmail.com>
2018-07-26 19:59:27 +0530

    Card notification template added to show notifications and slide nav added

---
+++ student/student.js
@@ -0,0 +1,11 @@
+// Write a JS function
+
+function askClassCode(){
+	var code = prompt("Please enter the class code");
+
+	if(code!=null)
+	{
+		alert("You have entered the code yay!" + " " +code);
+	}
+
+}
