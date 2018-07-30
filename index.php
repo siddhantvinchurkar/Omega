bf19f406bbbd90a93dd1dc9b567bb47e8723ebdc
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-07-31 01:11:17 +0530

    Added Landing Page

--- index.php
+++ index.php
@@ -1,217 +1,93 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
+<!DOCTYPE HTML>
+<html>
 	<head>
-		<!--Basic meta tags-->
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
-		<!--Fetch minified materializecss from the CDN asynchronously-->
-		<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css"  media="none" onload="if(media!='screen,projection')media='screen,projection'">
-
-		<!--Fetch material icons from the CDN asynchronously-->
-		<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" media="none" onload="if(media!='screen,projection')media='screen,projection'">
-
-		<!--Fetch font (Roboto) from the CDN asynchronously-->
-		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
-
-		<!--Set the shortcut icon-->
-		<link rel="shortcut icon" type="image/ico" href="images/icons/favicon.ico">
-
-		<!--Let browser know website is optimized for mobile-->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		<!--Progressive Web App meta tags-->
-		<link rel="manifest" href="manifest.json">
-		<meta name="theme-color" content="#EE6E73">
-		<meta name="Description" content="Omega is a collaborative classroom platform for teachers and students alike.">
-
-		<!--Overriden Styles-->
-		<style>
-			body {
-				display: flex;
-				min-height: 100vh;
-				flex-direction: column;
-				font-family: 'Roboto', sans-serif;
-				background-color: #F6F6F6;
-			}
-
-			main {
-				flex: 1 0 auto;
-			}
-
-			.container {
-				display: flex;
-				justify-content: center;
-			}
-
-			.center {
-				width: 250px;
-				position: absolute;
-				margin: auto;
-				top: 0;
-				right: 0;
-				bottom: 0;
-				left: 0;
-			}
-
-			.center2 {
-				opacity:0.1;
-				position: absolute;
-				margin: auto;
-				top: 0;
-				right: 0;
-				bottom: 0;
-				left: 0;
-			}
-
-		</style>
-
 		<title>Omega</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
+	<body class="is-preload">
 
-	<body>
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<h1>Omega</h1>
+					<p>Classrooms</p>
+				</header>
+				<footer>
+					<a href="#banner" class="button style2 scrolly-middle">Proceed as anticipated</a>
+				</footer>
+			</section>
 
-		<!--Header-->
-		<header>
+		<!-- Banner -->
+			<section id="banner">
+				<header>
+					<h2>This is Omega</h2>
+				</header>
+				<p>A brand new collaboration tool for students and teachers.<br />
+				It’s responsive, built on HTML5/CSS3, and entirely free<br />
+				under the <a href="https://omegaclassrooms.ga/LICENSE.md">MIT License</a>.</p>
+				<footer>
+					<a href="signin/" class="button style2 scrolly">Let me in!</a>
+				</footer>
+			</section>
 
-		<nav>
+		<!-- Feature 1 -->
+			<article id="first" class="container box style1 right">
+				<a href="#" class="image fit"><img src="https://media.istockphoto.com/vectors/group-of-students-vector-id910098436?k=6&m=910098436&s=612x612&w=0&h=ts1tIGl2J40iXoVrDAJHIIo1ah7uS9BotKx7qMQxeWg=" alt="" /></a>
+				<div class="inner">
+					<header>
+						<h2>Students</h2>
+					</header>
+					<p>Omega Classrooms enables students to take their entire classroom experience online. From assignments to notes. Everything is here.</p>
+				</div>
+			</article>
 
-			<div class="nav-wrapper">
-				<img src="images/icons/72.png" style="height:95%;" />
-				<a href="" class="brand-logo">Omega</a>
-				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a href="https://siddhantvinchurkar.me/" target="_blank" title="Siddhant Vinchurkar">Siddhant Vinchurkar</a></li>
+		<!-- Feature 2 -->
+			<article class="container box style1 left">
+				<a href="#" class="image fit"><img src="http://opportunityculture.org/wp-content/uploads/2017/07/rows-teachers-no-stars.jpg" alt="" /></a>
+				<div class="inner">
+					<header>
+						<h2>Teachers</h2>
+					</header>
+					<p>A fool-proof platform to manage assignment submissions, distribute course material and clarify doubts on the go.</p>
+				</div>
+			</article>
+
+		<!-- Portfolio -->
+			<article class="container box style2">
+				<header>
+					<h2>Open Source</h2>
+					<p>Omega is an open source software protected by the <a href="https://omegaclassrooms.ga/LICENSE.md">MIT License</a>. Feel free to fork the repository on <a href="https://github.com/siddhantvinchurkar/omega/" target="_blank">GitHub</a>!</p>
+				</header>
+			</article>
+
+		<section id="footer">
+			<ul class="icons">
+				<li><a href="https://github.com/siddhantvinchurkar/omega/" target="_blank" class="icon fa-github"><span class="label">GitHub</span></a></li>
+			</ul>
+			<div class="copyright">
+				<ul class="menu">
+					<li>&copy; <p id="year" style="display:inline;"></p> <a href="https://volatile.ga/">Volatile, Inc.</a> All rights reserved.</li><li>Designed by <a href="https://siddhantvinchurkar.me/">Siddhant Vinchurkar.</a></a></li>
 				</ul>
 			</div>
+		</section>
 
-		</nav>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
 
-		</header>
-
-		<!--Main-->
-		<main>
-
-			<!--Progrss Bar-->
-			<div class="container" id="progressBar">
-				<img src="images/icons/512.png" class="center2" />
-				<div class="progress center">
-					<div class="indeterminate" style="margin-right: auto; margin-left: auto; width: 250px;"></div>
-				</div>
-			</div>
-
-			<!--Contents-->
-			<div id="pageContents" style="display:none;">
-				<img src="images/icons/512.png" class="center2" />
-				<!--Card Panel [Info]-->
-				<div class="row">
-					<div class="col s12 m4" id="info1">
-						<div class="card-panel teal">
-							<span class="white-text">Welcome to <b>Omega</b>! Find all web tech lab programs here.</span>
-						</div>
-					</div>
-					<div class="col s12 m4" id="info2" style="display:none;">
-						<div class="card-panel teal">
-							<span class="white-text">Welcome to <b>Omega</b>! Find all web tech lab programs here.</span>
-						</div>
-					</div>
-					<div class="col s12 m4" id="info3" style="display:none;">
-						<div class="card-panel teal">
-							<span class="white-text">Welcome to <b>Omega</b>! Find all web tech lab programs here.</span>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-
-					<!--Card 2-->
-					<div class="col s12 m3">
-						<div class="card large">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="images/resume.jpg">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4" style="margin-top:1em;">HTML Resume<i class="material-icons right">arrow_upward</i></span>
-								<p>A responsive resume built in HTML5, CSS3 and a little bit of JavaScript.</p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">HTML Resume<i class="material-icons right">close</i></span>
-								<p>A responsive resume built in HTML5, CSS3 and a little bit of JavaScript.</p>
-								<img src="https://user-images.githubusercontent.com/30186107/29488525-f55a69d0-84da-11e7-8a39-5476f663b5eb.png" style="height:30%; width:80%;" />
-								<a href="" class="waves-effect waves-light btn"><i class="material-icons right">launch</i>Demo</a>
-								<p style="margin-top:2em;">Image Credits: <a style="float:right;background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@rawpixel?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from rawpixel"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg></span><span style="display:inline-block;padding:2px 3px">rawpixel</span></a></p>
-							</div>
-						</div>
-					</div>
-
-					<!--Card 2-->
-					<div class="col s12 m3">
-						<div class="card large">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="images/list.jpg">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4" style="margin-top:1em;">HTML Lists<i class="material-icons right">arrow_upward</i></span>
-								<p>Lists are very useful. This program explores the various types of list elements HTML5 offers.</p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">HTML Resume<i class="material-icons right">close</i></span>
-								<p>Lists are very useful. This program explores the various types of list elements HTML5 offers.</p>
-								<img src="https://user-images.githubusercontent.com/30186107/29488525-f55a69d0-84da-11e7-8a39-5476f663b5eb.png" style="height:30%; width:80%;" />
-								<a href="" class="waves-effect waves-light btn"><i class="material-icons right">launch</i>Demo</a>
-								<p style="margin-top:2em;">Image Credits: <a style="float:right;background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@glenncarstenspeters?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Glenn Carstens-Peters"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Glenn Carstens-Peters</span></a></p>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-		</main>
-
-		<!--Footer-->
-		<footer class="page-footer" style="padding-top:0px;">
-			<div class="footer-copyright">
-				<div class="container" id="footerText">
-					<a href="https://volatile.ga/" target="_blank" title="Volatile, Inc." style="color:#FFFFFF">&emsp;Volatile, Inc.&emsp;</a>
-					<a class="grey-text text-lighten-4 right" href="">Made with love for everyone</a>
-				</div>
-			</div>
-		</footer>
-
-		<!--JavaScript at end of body for optimized asynchronous loading-->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js" async></script>
-
-		<!--Onload Function-->
 		<script type="text/javascript">
-
 			window.onload = function(){
-
-				// Register a Service Worker
-				if('serviceWorker' in navigator) {
-				  navigator.serviceWorker
-				           .register('sw.js')
-				           .then(function() { console.log("Service Worker Registered"); });
-				}
-
-				// Set the copyright year in the footer
-				document.getElementById("footerText").innerHTML = '&copy; ' + new Date().getFullYear() + document.getElementById("footerText").innerHTML;
-
-				// Dismiss information panels on click
-				document.getElementById("info1").onclick = function(){document.getElementById("info1").style.display="none";};
-				document.getElementById("info2").onclick = function(){document.getElementById("info2").style.display="none";};
-				document.getElementById("info3").onclick = function(){document.getElementById("info3").style.display="none";};
-
-				// Get rid of the loader after three seconds
-				setTimeout(function(){
-					document.getElementById("progressBar").style.display="none";
-					document.getElementById("pageContents").style.display="block";
-					}, 3000);
-
+				document.getElementById("year").innerHTML = new Date().getFullYear();
 			}
-
 		</script>
 
 	</body>
-
 </html>
