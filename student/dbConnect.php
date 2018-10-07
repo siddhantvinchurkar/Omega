44e5a62fb2d764415b6361ea32f5b08886dc28cb
Nikhil <nik98hil@gmail.com>
2018-10-07 12:12:12 +0530

    db

--- student/dbConnect.php
+++ student/dbConnect.php
@@ -5,8 +5,8 @@ $password = "uglyhorse3449";
 $dbname = "omega";
 
 $conn= new mysqli($servername, $username, $password, $dbname);
-echo '<script>alert("I\'m here! 1");</script>';
 if(!$conn){
 die("Could not connect:".mysqli_connect_error());
 }
+echo '<script>alert("I\'m here! 1");</script>';
 ?>
