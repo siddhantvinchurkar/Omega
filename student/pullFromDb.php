4fcdf11c87d9f120a1eaea04d6c273f269867bfa
Nikhil <nik98hil@gmail.com>
2018-10-07 19:18:59 +0530

    db

--- student/pullFromDb.php
+++ student/pullFromDb.php
@@ -1,7 +1,7 @@
 <?php include 'dbConnect.php';
 
 //Networks
-$netAnn = 'SELECT * FROM master WHERE subject = "networks"';  
+$netAnn = 'SELECT * FROM announce WHERE subject = "networks"';  
 $retnet=mysqli_query($conn, $netAnn); 
 $ncount = mysqli_num_rows($retnet); 
 if($ncount > 0){  
@@ -12,49 +12,6 @@ echo "<tr><td>{$row['announce']}</td></tr>";
 echo "</table>";//end of while  
 }else{  	echo "no announcements";  
 }
-//Multimedia
-$mulAnn = 'SELECT * FROM master WHERE subject = "multimedia"';  
-$retmul = mysqli_query($conn, $mulAnn);  
-$mcount = mysqli_num_rows($retmul);
-
-if($mcount > 0){  
-	echo "<table><tr><th>Multimedia</th></tr>";
-while($row = mysqli_fetch_assoc($retmul)){ 
-echo "<tr><td>{$row['announce']}</td></tr>"; 
-} 
-echo "</table>";//end of while  
-}else{  	echo "no announcements";  
-}
-
-//COA
-$coaAnn = 'SELECT * FROM master WHERE subject = "coa"';  
-$retcoa = mysqli_query($conn, $coaAnn);
-$ccount = mysqli_num_rows($retcoa);  
-
-if($ccount > 0){  
-	echo "<table><tr><th>COA</th></tr>";
-while($row = mysqli_fetch_assoc($retcoa)){ 
-echo "<tr><td>{$row['announce']}</td></tr>";  
-} 
-echo "</table>";//end of while  
-}else{  	echo "no announcements";  
-}
-
-//web
-$webAnn = 'SELECT * FROM master WHERE subject = "web"';  
-$retweb = mysqli_query($conn, $webAnn);  
-$wcount = mysqli_num_rows($retweb);
-
-if($wcount > 0){  
-	echo "<table><tr><th>Web</th></tr>";
-while($row = mysqli_fetch_assoc($retweb)){ 
-echo "<tr><td>{$row['announce']}</td></tr>";  
-} 
-echo "</table>";//end of while  
-}else{  	echo "no announcements";  
-}
-
-
 mysqli_close($conn);  
 ?>
 
