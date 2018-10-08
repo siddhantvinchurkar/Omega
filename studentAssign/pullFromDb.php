fabe6103e0bafd3a175a17b7116d755db9023396
Nikhil <nik98hil@gmail.com>
2018-10-08 22:13:28 +0530

    announce bug solve

--- studentAssign/pullFromDb.php
+++ studentAssign/pullFromDb.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost","root","","omega");
+$conn = mysqli_connect("localhost","nikhil","password","omega");
 if(!$conn)
 	{
 		echo "Unable to connect to server: ".mysqli_connect_error();
