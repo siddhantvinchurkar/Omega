b833dc4efcc7baf3368422aa36dd30b506214c4f
All Users <shreyasharle919@gmail.com>
2018-10-08 23:49:03 +0530

    t1

--- teacher/index.php
+++ teacher/index.php
@@ -124,7 +124,7 @@
 					<div class="card">
 						<div class="card-image"> 
 							<img src="../images/list.jpg">
-							<a href="#" onclick="subjectOpen()"><span id="subjectName" class="card-title">Geography</span></a>
+							<a href="#" onclick="subjectOpen()"><span id="subjectName" class="card-title">Computer Graphics</span></a>
 						</div>
 						<div class="card-content">			
 							<div class="collection">
