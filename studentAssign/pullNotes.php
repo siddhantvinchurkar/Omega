f20b89209fc6fe2a331d0a5af7a16d152dcb5939
Nikhil <nik98hil@gmail.com>
2018-10-08 21:33:49 +0530

    notes

--- studentAssign/pullNotes.php
+++ studentAssign/pullNotes.php
@@ -1,13 +1,15 @@
 <?php 
 
-$conn = mysqli_connect("localhost","nikhil","password","omega");
+$conn = mysqli_connect("localhost","root","","omega");
 if(!$conn)
 	{
 		echo "Unable to connect to server: ".mysqli_connect_error();
 	}
 
+	echo "connected";
+
 $subjectN = $_GET["subname"];
-$sql = "SELECT * FROM announce WHERE subject = '$subjectN'"; 
+$sql = "SELECT * FROM notes"; 
 $retsub = mysqli_query($conn, $sql); 
 $ncount = mysqli_num_rows($retsub); 
 if($ncount > 0)
@@ -15,13 +17,14 @@ if($ncount > 0)
 	
 	while($row = mysqli_fetch_assoc($retsub))
 	{ 
-		echo "<tr><th>{$row['announcemnt']}</tr></th>";  
+		echo "<tr><th>{$row['link']}</tr></th>";  
 	} //end of while  
 }
 else
 {  	
 	echo "<p><h6>No announcements</h6></p>";  
 }
+
 mysqli_close($conn);  
 ?>
 
