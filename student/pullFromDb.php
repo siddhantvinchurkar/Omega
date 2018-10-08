91bf02284a4169b2448a550256e1a50f5c70f891
All Users <shreyasharle919@gmail.com>
2018-10-08 14:51:31 +0530

    trial

--- student/pullFromDb.php
+++ student/pullFromDb.php
@@ -4,13 +4,18 @@
 $netAnn = 'SELECT * FROM announce WHERE subject = "networks"'; 
 $retnet=mysqli_query($conn, $netAnn); 
 $ncount = mysqli_num_rows($retnet); 
-if($ncount > 0){  
+if($ncount > 0)
+{  
 	echo "<table><tr><th>Networks</th></tr>";
-while($row = mysqli_fetch_assoc($retnet)){ 
-echo "<tr><td>{$row['announce']}</td></tr>";  
-} 
-echo "</table>";//end of while  
-}else{  	echo " <p><h6>No announcements</h6></p>";  
+	while($row = mysqli_fetch_assoc($retnet))
+	{ 
+		echo "<tr><td>{$row['announce']}</td></tr>";  
+	} 
+	echo "</table>";//end of while  
+}
+else
+{  	
+	echo "<p><h6>No announcements</h6></p>";  
 }
 mysqli_close($conn);  
 ?>
