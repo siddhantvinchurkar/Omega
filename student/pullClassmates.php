5209b80b2547bd504b0a23d21f16c4452e3963aa
Nikhil <nik98hil@gmail.com>
2018-10-09 08:19:29 +0530

    class

--- student/pullClassmates.php
+++ student/pullClassmates.php
@@ -5,19 +5,19 @@ if(!$conn)
 		echo "Unable to connect to server: ".mysqli_connect_error();
 	}
 
-$sql = 'SELECT * FROM users WHERE usertype = "S"'; 
+$sql = "SELECT * FROM users"; 
 $retsub = mysqli_query($conn, $sql); 
 $ncount = mysqli_num_rows($retsub); 
 if($ncount > 0)
 {  
-	echo "<tr><th>First Name</th></tr>
-			<tr><th><Last Name</th></tr>
-				<tr><th>Email</th></tr>";
+	echo "<tr><th>First Name</th>
+			<th><Last Name</th>
+				<th>Email</th></tr>";
 	while($row = mysqli_fetch_assoc($retsub))
 	{ 
-		echo "<tr><td>{$row['fn']}</tr></td>
-				<tr><td>{$row['ln']}</tr></td>
-					<tr><td>{$row['eml']}</tr></td>";  
+		echo "<tr><td>{$row['fn']}</td>
+				<td>{$row['ln']}</td>
+					<td>{$row['eml']}</td></tr>";  
 	} //end of while  
 }
 else
